51c279c53abe3f6d0cf3c56f320b0a300e0d578f
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-11-01 14:07:42 +0300

    [js] symlinks to vendor libs

--- js/vendor/mobile-detect-modernizr.js
+++ js/vendor/mobile-detect-modernizr.js
@@ -1,15 +1,1 @@
-/*global Modernizr MobileDetect*/
-
-(function (window, Modernizr) {
-    'use strict';
-    var md = new MobileDetect(navigator.userAgent),
-        grade = md.mobileGrade();
-    Modernizr.addTest({
-        mobile: !!md.mobile(),
-        phone: !!md.phone(),
-        tablet: !!md.tablet(),
-        mobilegradea: grade === 'A'
-    });
-    window.mobileDetect = md;
-})(window, Modernizr);
-
+../../bower_components/mobile-detect/mobile-detect-modernizr.js
