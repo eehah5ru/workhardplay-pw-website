6b3837576c2061fe5038dbacafffebdfa62ca7c4
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-11-10 22:22:38 +0300

    [engine][slim] if,for hakyll wrappers

--- slimlib.rb
+++ slimlib.rb
@@ -1,11 +1,53 @@
 require "slim/include"
 
+Slim::Engine.set_options(:disable_escape => true)
+
+#
+#
+# hakyll template wrapper
+#
+#
 def field name
   return "$#{name.to_s}$"
 end
 
+def _if cond, &block
+  return "$if(#{cond})$\n#{yield}\n"
+end
+
+def _else &block
+  return "$else$\n#{yield}\n"
+end
+
+def _endif
+  return "$endif$\n"
+end
+
+def _for stmt, &block
+  return "$for(#{stmt})$\n#{yield}\n"
+end
+
+def _endfor
+  return "$endfor$\n"
+end
+
+#
+#
+# url helpers
+#
+#
 def archive_image(file_name)
   return "/images/2016/archive/$canonical_name$/" + file_name
+end
+
+#
+#
+# styles
+#
+#
+
+def class_has_video
+  return "$if(has_video)$has-video-icon$endif$"
 end
 
 def schedule_prj_cover_style(project_name, image_path)
