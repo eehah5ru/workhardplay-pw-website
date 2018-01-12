4681694b3a51281ecde2501885efd83482dd64c6
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2018-01-12 17:37:26 +0300

    shared components were extracted to w7w-core project

--- src/Site/Compilers/Markdown.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Site.Compilers.Markdown where
-
-import Hakyll
-
-markdownPageRules :: (Item String -> Compiler (Item String)) -> Rules ()
-markdownPageRules f = do
-  route $ setExtension "html"
-  compile $ pandocCompiler >>= f
--- src/Site/Types.hs
+++
@@ -1,7 +0,0 @@
-module Site.Types where
-
-import           Hakyll
-
-
-type TemplateIdentifier = Identifier
-type PageIdentifier = Identifier
