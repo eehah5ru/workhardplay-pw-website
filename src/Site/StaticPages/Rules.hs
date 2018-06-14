d49983898764c3f661d3d395c6e1f29c75b1153f
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2018-06-14 14:58:29 +0300

    [engine][2018] set deps for 2018's index page

--- src/Site/StaticPages/Rules.hs
+++ src/Site/StaticPages/Rules.hs
@@ -2,6 +2,12 @@
 
 module Site.StaticPages.Rules where
 
+import           Data.Monoid (mappend, (<>))
+
+import Hakyll
+
+import W7W.MultiLang
+
 import W7W.Rules.StaticPages
 
 import Site.Template
@@ -10,7 +16,8 @@ import Site.Context
 staticPagesRules = do
   rulesIndex "index.md"
   rulesAbout "about.md"
-  rulesSlim "2018/index.slim"
+  with2018deps (rulesSlim "2018/index.slim")
+
   rulesSlim "2017/index.slim"
   rulesSlim "2017/404.slim"
   rulesSlim "2016/index.slim"
@@ -18,3 +25,7 @@ staticPagesRules = do
     rulesSlim = staticSlimPageRules rootTpl (Just rootPageTpl) Nothing siteCtx
     rulesAbout = staticPandocPageRules rootTpl (Just rootPageTpl) (Just "templates/about.slim") siteCtx
     rulesIndex = staticPandocPageRules rootTpl (Just rootPageTpl) (Just "templates/index.slim") siteCtx
+    with2018deps rules = do
+      let lDeps = \l -> (fromGlob (localizePath l "2018/shared/_*.slim"))
+      deps <- makePatternDependency $ ("ru/**/_*.slim" .||. "en/**/_*.slim")
+      rulesExtraDependencies [deps] rules
