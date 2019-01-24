164eb512dc1a6239649de24b489758fbd93c671e
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-01-24 17:55:22 +0100

    squash! [vagrant] master/slave multimachine env

---
+++ provisioning/whph-common-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+rvm install `cat /home/vagrant/whph-website/.ruby-version`
+
+cd /home/vagrant/whph-website
+
+rvm . do gem install bundler
+
+rvm . do bundle install
+
+nvm install `cat .nvmrc`
+
+nvm use
+
+npm install
+
+# FIXME: nothing saved in bower.json
+npx bower install
