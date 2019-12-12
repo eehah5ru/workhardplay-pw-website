3ef76bc620ebb1560e89e8d760e1e40cef04590e
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-12-12 15:45:05 +0300

    [engine][terms] add simple and yet not working terms checker

--- src/Site/CollectiveGlossary.hs
+++ src/Site/CollectiveGlossary.hs
@@ -4,6 +4,9 @@ module Site.CollectiveGlossary where
 
 import           Data.Monoid (mappend, (<>))
 
+import           Control.Monad                   (foldM, forM, forM_, mplus)
+import qualified Data.Set                        as S
+import qualified Data.Map                        as M
 import Data.Maybe (fromMaybe)
 import Data.List (find)
 import Hakyll
@@ -25,16 +28,51 @@ terms RU = ruTerms
 terms EN = enTerms
 terms _ = error "unknown locale"
 
+
+-- buildTags' :: MonadMetadata m => Pattern -> (Identifier -> String -> Identifier) -> m Tags
+-- buildTags' pattern makeId = do
+--     ids    <- getMatches pattern
+--     tagMap <- foldM addTags M.empty ids
+--     let set' = S.fromList ids
+--     return $ Tags (M.toList tagMap) makeId (PatternDependency pattern set')
+--   where
+--     -- Create a tag map for one page
+--     addTags tagMap id' = do
+--         tags <- getTags id'
+--         let tagMap' = M.fromList $ zip tags $ repeat [id']
+--         return $ M.unionWith (++) tagMap tagMap'
+
+checkTags :: Locale -> [(Identifier, Metadata)] -> Tags -> Rules ()
+checkTags l termsMetadata tags = do
+  mapM_ checkTag' (tagsMap tags)
+
+  where
+    checkDocTag' :: String -> Identifier -> Rules ()
+    checkDocTag' tagName docId = do
+      let e = error  $ (show docId) ++ ": there is no defenition for '" ++ tagName ++ "'"
+          mi = termToIdentifierMaybe l termsMetadata tagName
+        in return (fromMaybe e mi) >> return ()
+
+
+    checkTag' :: (String, [Identifier]) -> Rules ()
+    checkTag' (tagName, docIds) = do
+      mapM_ (checkDocTag' tagName) docIds
+
+
 buildTerms :: Rules Terms
 buildTerms = do
   mRu <- getAllMetadata "ru/collective-glossary/*.md"
   mEn <- getAllMetadata "en/collective-glossary/*.md"
 
-  ru <- buildTags' mRu RU
-  en <- buildTags' mEn EN
-  return $ mkTerms ru en
+  ruTags <- buildTags'' mRu RU
+  enTags <- buildTags'' mEn EN
+
+  checkTags RU mRu ruTags
+  checkTags EN mEn enTags
+
+  return $ mkTerms ruTags enTags
   where
-    buildTags' m l = buildTags
+    buildTags'' m l = buildTags
                        termsSources
                        (termToIdentifier l m)
       where
@@ -55,10 +93,15 @@ buildTerms = do
 termToIdentifier :: Locale -> [(Identifier, Metadata)] -> String -> Identifier
 termToIdentifier l ms term =
   let e = error  $ "there is no defenition for '" ++ term ++ "'"
-      mi =  findTermCanonicalName ms term >>= return . fromCapture (fromGlob $ localizePath l "collective-glossary/*.md")
+      mi = termToIdentifierMaybe l ms term
   in fromMaybe e mi
 
-  where
+
+termToIdentifierMaybe :: Locale -> [(Identifier, Metadata)] -> String -> Maybe Identifier
+termToIdentifierMaybe l ms term =
+ findTermCanonicalName ms term >>= return . fromCapture (fromGlob $ localizePath l "collective-glossary/*.md")
+
+ where
 
     testF :: String -> (Identifier, Metadata) -> Bool
     testF term (i, m) =
